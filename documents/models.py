af2ccf58b7141c946a7fcbb6e568da9289853a48
Issra Hashim <issrahashim@hotmail.fr>
2022-02-02 14:09:10 +0000

    added basic app

--- documents/models.py
+++ documents/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Document(models.Model):
     title = models.CharField(max_length=50)
-    file = models.FileField(upload_to='documents/media')
+    file = models.FileField()
 
     def __str__(self):
         return f'{self.title}'
--- documents/views.py
+++ documents/views.py
@@ -12,15 +12,15 @@ from .serializers import DocumentSerializer
 
 class DocumentListView(APIView):
 
-    def upload_file(request):
-        if request.method == 'POST':
-            form = DocumentForm(request.POST, request.FILES)
-            if form.is_valid():
-                form.save()
-                return redirect('')
-        else:
-            form = DocumentForm()
-        return render(request, 'upload.html', {'form': form})
+    # def upload_file(request):
+    #     if request.method == 'POST':
+    #         form = DocumentForm(request.POST, request.FILES)
+    #         if form.is_valid():
+    #             form.save()
+    #             return redirect('')
+    #     else:
+    #         form = DocumentForm()
+    #     return render(request, 'upload.html', {'form': form})
 
     def get(self, _request):
         documents = Document.objects.all()
@@ -33,12 +33,26 @@ class DocumentDetailView(APIView):
         try:
             document = Document.objects.get(id=pk)
             serialized_document = DocumentSerializer(document)
-            url = serialized_document.data['file'].replace('/documents/media/', '')
-            f = open(f'{url}', 'r')
-            if f.mode == 'r':
-                contents = f.read()
-                print(contents)
-            return Response(contents, status=status.HTTP_200_OK)
+            url = serialized_document.data['file'].replace('/', '')
+            print(url)
+            g = open(url, 'r')
+            print(g.readable())
+            print(g.read())
+            g.seek(0)
+            print(g.readline())
+            print(g.readlines())
+            # with open('media/doc1_QqnKXb1.txt') as fp:
+            #     line = fp.readline()
+            #     cnt = 1
+            #     while line:
+            #         print("Line {}: {}".format(cnt, line.strip()))
+            #         line = fp.readline()
+            #         cnt += 1
+            # f = open(f'{url}', 'r')
+            # if f.mode == 'r':
+            #     contents = f.read()
+            #     print(contents)
+            return Response(status=status.HTTP_200_OK)
         except:
             print('error')
             return Response(status=status.HTTP_404_NOT_FOUND)
