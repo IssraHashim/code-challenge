39dbf2b196decc097470be5e165e6034173e2ede
Issra Hashim <issrahashim@hotmail.fr>
2022-02-02 13:32:57 +0000

    set up done

---
+++ documents/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Document(models.Model):
+    title = models.CharField(max_length=50)
+    file = models.FileField(upload_to='documents/media')
+
+    def __str__(self):
+        return f'{self.title}'
---
+++ documents/views.py
@@ -0,0 +1,45 @@
+from pydoc import doc
+from django.shortcuts import redirect, render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.files import File
+from documents.forms import DocumentForm
+from .models import Document
+from .serializers import DocumentSerializer
+
+
+
+class DocumentListView(APIView):
+
+    def upload_file(request):
+        if request.method == 'POST':
+            form = DocumentForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                return redirect('')
+        else:
+            form = DocumentForm()
+        return render(request, 'upload.html', {'form': form})
+
+    def get(self, _request):
+        documents = Document.objects.all()
+        serialized_documents = DocumentSerializer(documents, many=True)
+        return Response(serialized_documents.data, status=status.HTTP_200_OK)
+
+
+class DocumentDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            document = Document.objects.get(id=pk)
+            serialized_document = DocumentSerializer(document)
+            url = serialized_document.data['file'].replace('/documents/media/', '')
+            f = open(f'{url}', 'r')
+            if f.mode == 'r':
+                contents = f.read()
+                print(contents)
+            return Response(contents, status=status.HTTP_200_OK)
+        except:
+            print('error')
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
