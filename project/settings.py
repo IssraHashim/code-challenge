2253fcf849a9b20a5bb4ad1c9673996613d90633
Issra Hashim <issrahashim@hotmail.fr>
2022-02-02 14:18:18 +0000

    secured doc

--- project/settings.py
+++ project/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@7skt7z!=srgn@8y(v!ygkxgtgpbe+__8wr09d&f23@#c*i%x'
-
+# from decouple import config
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
